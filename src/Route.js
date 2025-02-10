277497fa229fcbb7fbb18b401833d5e284816cd3
manjuajgowda <152592250+manjuajgowda@users.noreply.github.com>
2025-02-10 22:35:51 +0530

    changes

--- src/Route.js
+++ src/Route.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
 } from "react-router-dom";
 import { Footer } from "./Layouts/Footer";
 import { Header } from "./Layouts/Header";
